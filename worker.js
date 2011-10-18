285abfb36b2ebfa61245c94664d51b505ef7e29d
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2011-10-18 19:36:25 +0900

    Close #2

--- worker.js
+++ worker.js
@@ -26,16 +26,23 @@ request.addListener('response', function (response) {
     if (chunk.match(/^\s*$/)) {
       return;
     }
+
     var tweet = JSON.parse(chunk);
+
+    if (tweet.user.id != EEW_JP_ID) {
+      return;
+    }
+
     oauth.post(
       'http://api.twitter.com/1/statuses/update.json',
       process.env.ACCESS_TOKEN,
       process.env.ACCESS_TOKEN_SECRET,
-      { status: 'ゆれゆれ、はじまるよー！ http://twitter.com/' + tweet.user['screen_name'] + '/status/' + tweet.id_str },
+      { status: 'ゆるゆれ、はじまるよー！ http://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str },
       function(err, data) {
         err && console.log(err);
       }
     );
   });
 });
+
 request.end();
